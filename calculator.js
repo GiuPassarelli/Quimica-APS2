b1c674c38ef867c2640029a7a9dc01c0961c29cb
GiuPassarelli <gb_passarelli@hotmail.com>
2019-04-18 10:28:53 -0300

    Montar e Selecionar done

---
+++ calculator.js
@@ -0,0 +1,5 @@
+function showDiv(divIdHide, divIdShow, style, subtitle){
+    document.getElementById(divIdHide).style.display = 'none';
+    document.getElementById(divIdShow).style.display = style;
+    document.getElementById("subtitle").innerHTML = subtitle;
+}
